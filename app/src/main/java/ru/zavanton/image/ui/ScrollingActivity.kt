e917a4d0b10a888ada5e223ef57b70e65cdda082
zavanton <zavanton@yandex-team.ru>
2020-06-05 11:40:03 +0300

    scrolling activity added

---
+++ app/src/main/java/ru/zavanton/image/ui/ScrollingActivity.kt
@@ -0,0 +1,20 @@
+package ru.zavanton.image.ui
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_scrolling.*
+import ru.zavanton.image.R
+
+class ScrollingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrolling)
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
