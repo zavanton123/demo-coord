16c194b137a7aff89ccbb1b236ed6786ee6df5c0
zavanton <zavanton@yandex-team.ru>
2020-06-06 10:10:51 +0300

    small fabs added

--- app/src/main/java/ru/zavanton/image/extensions/Extensions.kt
+++ app/src/main/java/ru/zavanton/image/extensions/Extensions.kt
@@ -1,8 +1,14 @@
 package ru.zavanton.image.extensions
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.content.Context
 import android.util.TypedValue
 import android.view.View
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 fun Context.dpToPx(dp: Int): Float {
     return TypedValue.applyDimension(
@@ -30,4 +36,32 @@ fun View.visible() {
 
 fun View.invisible() {
     this.visibility = View.INVISIBLE
+}
+
+fun FloatingActionButton.animateWithTranslation(trX: Float, trY: Float) {
+    val animator = ObjectAnimator.ofPropertyValuesHolder(
+        this,
+        PropertyValuesHolder.ofFloat("translationX", this.translationX, trX),
+        PropertyValuesHolder.ofFloat("translationY", this.translationY, trY)
+    ).apply {
+        duration = 300
+        interpolator = FastOutSlowInInterpolator()
+    }
+    animator.start()
+}
+
+fun FloatingActionButton.init(trX: Float, trY: Float) {
+    visibility = View.INVISIBLE
+    addOnShowAnimationListener(object : AnimatorListenerAdapter() {
+        override fun onAnimationStart(animation: Animator?) {
+            animateWithTranslation(trX, trY)
+            super.onAnimationStart(animation)
+        }
+    })
+    addOnHideAnimationListener(object : AnimatorListenerAdapter() {
+        override fun onAnimationStart(animation: Animator?) {
+            animateWithTranslation(0f, 0f)
+            super.onAnimationStart(animation)
+        }
+    })
 }
--- app/src/main/java/ru/zavanton/image/ui/MainActivity.kt
+++ app/src/main/java/ru/zavanton/image/ui/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.zavanton.image.R
 import ru.zavanton.image.extensions.gone
+import ru.zavanton.image.extensions.init
 import ru.zavanton.image.extensions.visible
 
 class MainActivity : AppCompatActivity() {
@@ -14,12 +15,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupFabs()
+
         articleSubMenu.setOnClickListener {
             toggleCustomBottomBar()
         }
 
         customBottomBar.setOnClickListener {
             toggleArticleSubMenu()
+        }
+    }
+
+    private fun setupFabs() {
+        mini1.init(-96f, -96f)
+        mini2.init(0f, -128f)
+        mini3.init(96f, -96f)
+
+        fab.setOnClickListener {
+            if (mini1.isOrWillBeShown) mini1.hide() else mini1.show()
+            if (mini2.isOrWillBeShown) mini2.hide() else mini2.show()
+            if (mini3.isOrWillBeShown) mini3.hide() else mini3.show()
         }
     }
 
